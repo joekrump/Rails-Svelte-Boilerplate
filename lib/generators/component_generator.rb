c89670b3a1367f71eda106561a5d40d2d0e1c6a4
Joe Krump <joe.krump@clio.com>
2020-11-23 22:25:38 -0800

    Update component generator to register component

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -3,7 +3,7 @@ class ComponentGenerator < Rails::Generators::Base
   argument :name, required: true, desc: "Component name, e.g: button"
 
   def create_svelte_file
-    create_file "#{component_path}/#{component_name}.svelte" do
+    create_file component_path do
       <<~content
         <style>
           /* */
@@ -16,6 +16,16 @@ class ComponentGenerator < Rails::Generators::Base
         </script>
       content
     end
+
+    inject_into_file 'frontend/packs/application.js', after: "import WebpackerSvelte from \"@coding-g/webpacker-svelte\";\n" do
+      <<~content
+        import #{component_import_name} from "#{component_application_import_path}";
+      content
+    end
+
+    inject_into_file 'frontend/packs/application.js', after: "WebpackerSvelte.setup({\n" do
+      "  #{component_import_name},\n"
+    end
   end
 
   protected
@@ -24,7 +34,24 @@ class ComponentGenerator < Rails::Generators::Base
     @component_name ||= name.underscore.dasherize
   end
 
+  def component_import_name
+    import_name = ""
+    name_parts = component_name.split("-")
+    name_parts.each do |part|
+      import_name << part.capitalize
+    end
+    import_name
+  end
+
+  def component_application_import_path
+    "../components/#{component_name}/#{component_name}.svelte"
+  end
+
+  def components_root_path
+    "frontend/components"
+  end
+
   def component_path
-    "frontend/components/#{component_name}"
+    "#{components_root_path}/#{component_name}/#{component_name}.svelte"
   end
 end
