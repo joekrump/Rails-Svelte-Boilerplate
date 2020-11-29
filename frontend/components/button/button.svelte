bcaf32a1e36ee84883fc91bbe18c0d1a413fb139
Joe Krump <joe.krump@clio.com>
2020-11-28 20:38:50 -0800

    Add onClick handler for button

--- frontend/components/button/button.svelte
+++ frontend/components/button/button.svelte
@@ -1,10 +1,12 @@
 <button type={type}
   class={`inline-flex items-center justify-center px-5 py-3 border-b-4 leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 transition duration-150 ease-in-out ${classes}`}
+  on:click={onClick}
 >
   <slot></slot>
 </button>
 
-<script>
-  export let type;
-  export let classes;
+<script lang="ts">
+  export let type: string;
+  export let classes: string;
+  export let onClick: svelte.JSX.EventHandler = () => null;
 </script>
